f60bae4e8503134ad9713d4418d440291196a4a1
mrcodefrost <mr.codefrost@gmail.com>
2024-11-14 18:48:52 +0530

    Modal done

--- src/components/custom-modal-popup/Modal.jsx
+++ src/components/custom-modal-popup/Modal.jsx
@@ -1,14 +1,36 @@
-const Modal = () => {
+import './Modal.css';
+
+
+const Modal = ({ id, header, body, footer, onClose }) => {
   return (
-    <div id = {id || 'Modal'} className="modal">
+    <div id={id || 'Modal'} className="modal">
+      {/* show header, content, footer of modal */}
 
-    <div className="content">
-        
-    </div>
+      <div className="modal-content">
 
-    {/* show header, content, footer of modal */}
+        <div className="header">
+          <span onClick={onClose} className="close-modal-icon">&times;</span>
+          <h2>{header ? header : 'Header'}</h2>
+        </div>
 
+        <div className="body">
+          {
+            body ? (body) : (
+              <div>
+                <p>This is our modal body</p>
+              </div>
+            )
+          }
+        </div>
+        <div className="footer">
+          {
+            footer ? footer : <h2>
+              Footer
+            </h2>
+          }
+        </div>
 
+      </div>
 
     </div>
   )
--- src/components/custom-modal-popup/ModalTest.jsx
+++ src/components/custom-modal-popup/ModalTest.jsx
@@ -2,19 +2,22 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 export default function ModalTest() {
-    
+
     const [showModalPopup, setShowModalPopup] = useState(false);
 
     function handleToggleModalPopup() {
         setShowModalPopup(!showModalPopup);
     }
 
+    function onClose() {
+        setShowModalPopup(false);
+    }
+
     return <div>
         <button onClick={handleToggleModalPopup} >Open Modal Popup</button>
         {
-
             // if showModalPopup is true then show Modal
-            showModalPopup && <Modal />
+            showModalPopup && <Modal onClose={onClose} body={<p>This is our body</p>} />
         }
     </div>
 
