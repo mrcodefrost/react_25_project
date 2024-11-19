ae9fd550ff6a6c6ef63833756f57db9b8c150309
mrcodefrost <mr.codefrost@gmail.com>
2024-11-19 18:38:53 +0530

    tic tac toe starter

---
+++ src/components/tic-tac-toe/TicTacToe.jsx
@@ -0,0 +1,20 @@
+const TicTacToe = () => {
+  return (
+    <div className="tic-tac-toe-container">
+    
+    <div className="row">
+
+    </div>
+
+    <div className="row">
+
+    </div>
+
+    <div className="row">
+
+    </div>
+
+    </div>
+  )
+}
+export default TicTacToe
