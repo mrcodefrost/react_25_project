4d7778fe0c2431515481c0d02ffecfc8d24ab49c
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2024-12-07 20:03:45 +0530

    added fetchFeature

--- src/components/feature-flags/context/ContextFeatureFlag.jsx
+++ src/components/feature-flags/context/ContextFeatureFlag.jsx
@@ -1,9 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const FeatureFlagsContext = createContext(null);
 
 export default function FeatureFlagGlobalState({ children }) {
+    const [loading, setLoading] = useState(false);
+    const [enabledFlags, setEnabledFlags] = useState({});
+
+
+    async function fetchFeatureFlags() {
+        try {
+            const respone = await featureFlagsDataServiceCall
+            console.log(respone);
+        }
+        catch (error) {
+            console.log(error);
+            throw new Error(error);
+        }
+    }
+
+
+    useEffect(
+        () => {
+
+            fetchFeatureFlags();
+        }, [])
     return (
         <FeatureFlagsContext.Provider value={{}}>
             {children}
